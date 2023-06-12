320c036049d58b0234f6b503804670f725b743df
Aditya Kitukale <126107933+Adityakitukale499@users.noreply.github.com>
2023-06-12 11:47:46 +0530

    Add files via upload

---
+++ src/component/Basics/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = ({ filterItem, menuList }) => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem, i) => {
+            return (
+              <button key={i} className="btn-group__item"
+                onClick={() => filterItem(curElem)}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
